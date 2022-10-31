6172f175dd11308bd2399a3754207f5fb06e24f7
lletierce <letierce.lo@gmail.com>
2022-10-31 15:25:36 +0100

    Add automapper for UpdateCharacter

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -54,12 +54,13 @@ namespace dotnet_rpg.Services.CharacterService
             {
                 Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
 
-                character.Name = updatedCharacter.Name;
-                character.HitPoints = updatedCharacter.HitPoints;
-                character.Strength = updatedCharacter.Strength;
-                character.Defense = updatedCharacter.Defense;
-                character.Intelligence = updatedCharacter.Intelligence;
-                character.Class = updatedCharacter.Class;
+                _mapper.Map(updatedCharacter, character);
+                // character.Name = updatedCharacter.Name;
+                // character.HitPoints = updatedCharacter.HitPoints;
+                // character.Strength = updatedCharacter.Strength;
+                // character.Defense = updatedCharacter.Defense;
+                // character.Intelligence = updatedCharacter.Intelligence;
+                // character.Class = updatedCharacter.Class;
 
                 response.Data = _mapper.Map<GetCharacterDto>(character);
             }
